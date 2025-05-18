d32913158911bcac09f0a33420e65e862261b33e
Sleepy Machine <acooper2501@gmail.com>
2025-05-17 20:54:14 -0700

    Initial commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,21 @@
+
+//Allows access to all assets, classes, and functions within the Unity Engine.
+using UnityEngine;
+
+// The class HelloWorld inherits from MonoBehaviour using the : sign, which is the base class for unity scripts.
+// This is also a public class, allowing it to be accessed by other objects or scripts in the program.
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//Adding this comment line to test commiting changes to GitHub
